8a585b386232cbca6531c2c3e8bb1cfb4cc0f863
Conchsk <Conchsk@outlook.com>
2018-09-03 09:30:16 +0800

    20180903

--- main.py
+++ main.py
@@ -1,18 +1,49 @@
+import time
+
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.datasets import make_classification, make_moons, make_circles
+from sklearn.datasets import make_circles, make_classification, make_moons
+from sklearn.svm import SVC
 
 import mysvm
 
-X, y = make_circles(noise=0.1, random_state=0, factor=0.5)
+mysvm_plt = plt.subplot(1, 2, 1)
+sksvm_plt = plt.subplot(1, 2, 2)
+cm = plt.cm.RdBu
+dist = 'circle'
+
+if dist == 'linear':
+    X, y = make_classification(n_features=2, n_redundant=0,
+                               n_clusters_per_class=1, random_state=1)
+    X += 2 * np.random.RandomState(2).uniform(X.size)
+elif dist == 'moon':
+    X, y = make_moons(noise=0.2, random_state=0)
+else:
+    X, y = make_circles(noise=0.2, factor=0.5, random_state=1)
+
 yt = y.copy()
 yt[yt == 0] = -1
-model = mysvm.MySVM().fit(X, yt, 10.0, 1e-3, 'rbf')
-print(model._alphas)
 xx, yy = np.meshgrid(np.arange(X[:, 0].min() - 0.5, X[:, 0].max() + 0.5, 0.1),
                      np.arange(X[:, 1].min() - 0.5, X[:, 1].max() + 0.5, 0.1))
-Z = np.apply_along_axis(lambda it: model.predict(it), 1, np.c_[xx.ravel(), yy.ravel()])
-Z = Z.reshape(xx.shape)
-plt.contourf(xx, yy, Z, cmap=plt.cm.RdBu, alpha=0.8)
-plt.scatter(X[:, 0], X[:, 1], c=y)
+# mysvm
+mysvm_start_time = time.time()
+mysvm_model = mysvm.MySVM().fit(X, yt, 1, 1e-3, 'rbf', gamma=2)
+mysvm_Z = np.apply_along_axis(lambda it: mysvm_model.predict(it),
+                              1, np.c_[xx.ravel(), yy.ravel()])
+mysvm_stop_time = time.time()
+print(mysvm_stop_time - mysvm_start_time)
+mysvm_Z = mysvm_Z.reshape(xx.shape)
+mysvm_plt.contourf(xx, yy, mysvm_Z, cmap=cm, alpha=0.8)
+mysvm_plt.scatter(X[:, 0], X[:, 1], c=y)
+
+# sksvm
+sksvm_start_time = time.time()
+sksvm_model = SVC(gamma=2, C=1).fit(X, y)
+sksvm_Z = sksvm_model.decision_function(np.c_[xx.ravel(), yy.ravel()])
+sksvm_Z = sksvm_Z.reshape(xx.shape)
+sksvm_stop_time = time.time()
+print(sksvm_stop_time - sksvm_start_time)
+sksvm_plt.contourf(xx, yy, sksvm_Z, cmap=cm, alpha=0.8)
+sksvm_plt.scatter(X[:, 0], X[:, 1], c=y)
+
 plt.show()
--- mysvm.py
+++ mysvm.py
@@ -3,30 +3,39 @@ import math
 
 
 class MySVM:
-    def fit(self, features: np.ndarray, labels: np.ndarray, C: float, tol: float, kernel: str):
+    def fit(self, features: np.ndarray, labels: np.ndarray, C: float, tol: float, kernel: str, gamma: float = 1.0,
+            max_iter: int = 1000):
         self._features = features
         self._labels = labels
         self._C = C
         self._tol = tol
         self._kernel = kernel
+        self._gamma = gamma
+        self._max_iter = max_iter
 
         self._smo()
         return self
 
     def predict(self, feature: np.ndarray) -> float:
-        return self._calcU(feature)
+        return self._calc_u2(feature)
 
-    def _kernelCompute(self, x1: np.ndarray, x2: np.ndarray) -> float:
+    def _kernel_compute(self, x1: np.ndarray, x2: np.ndarray) -> float:
         if self._kernel == 'linear':
             return x1.dot(x2)
+        elif self._kernel == 'poly':
+            return pow(1 + x1.dot(x2), self._gamma)
         elif self._kernel == 'rbf':
-            return math.exp(-np.linalg.norm(x1 - x2) / 200)
+            return math.exp(-self._gamma * np.linalg.norm(x1 - x2))
 
-    def _calcU(self, feature: np.ndarray) -> float:
-        kxx = np.apply_along_axis(lambda it: self._kernelCompute(it, feature), 1, self._features)
+    def _calc_u1(self, i: int) -> float:
+        return (self._labels * self._alphas).dot(self._kxx[i]) - self._b
+
+    def _calc_u2(self, feature: np.ndarray) -> float:
+        kxx = np.apply_along_axis(lambda it: self._kernel_compute(
+            it, feature), axis=1, arr=self._features)
         return (self._labels * self._alphas).dot(kxx) - self._b
 
-    def _takeStep(self, i1: int, i2: int) -> int:
+    def _take_step(self, i1: int, i2: int) -> int:
         if i1 == i2:
             return 0
         X1 = self._features[i1]
@@ -35,48 +44,50 @@ class MySVM:
         y2 = self._labels[i2]
         alph1 = self._alphas[i1]
         alph2 = self._alphas[i2]
-        E1 = self._calcU(X1)-y1
-        E2 = self._calcU(X2)-y2
+        E1 = self._calc_u1(i1) - y1
+        E2 = self._calc_u1(i2) - y2
 
         L = 0.0
         H = 0.0
-        s = y1*y2
+        s = y1 * y2
         if s == -1.0:
-            L = max(0, alph2-alph1)
-            H = min(self._C, self._C+alph2-alph1)
+            L = max(0, alph2 - alph1)
+            H = min(self._C, self._C + alph2 - alph1)
         else:
-            L = max(0, alph2+alph1-self._C)
-            H = min(self._C, alph2+alph1)
-        if abs(L-H) < self._eps:
+            L = max(0, alph2 + alph1 - self._C)
+            H = min(self._C, alph2 + alph1)
+        if abs(L - H) < self._eps:
             return 0
 
-        k11 = self._kernelCompute(X1, X1)
-        k12 = self._kernelCompute(X1, X2)
-        k22 = self._kernelCompute(X2, X2)
-        eta = k11+k22-2*k12
+        k11 = self._kxx[i1, i1]
+        k12 = self._kxx[i1, i2]
+        k22 = self._kxx[i2, i2]
+        eta = k11 + k22 - 2 * k12
 
         a2 = 0.0
         if eta > 0:
-            a2 = alph2+y2*(E1-E2)/eta
+            a2 = alph2 + y2 * (E1 - E2) / eta
             if a2 < L:
                 a2 = L
             elif a2 > H:
                 a2 = H
         else:
-            f1 = y1*(E1+self._b)-alph1*k11-s*alph2*k12
-            f2 = y2*(E2+self._b)-s*alph1*k12-alph2*k22
-            L1 = alph1 + s * (alph2-L)
-            H1 = alph1 + s * (alph2-H)
-            PsiL = L1 * f1 + L * f2 + 0.5 * L1 * L1 * k11 + 0.5 * L * L * k22 + s * L * L1 * k12
-            PsiH = H1 * f1 + H * f2 + 0.5 * H1 * H1 * k11 + 0.5 * H * H * k22 + s * H * H1 * k12
-            if PsiL < PsiH:
+            f1 = y1 * (E1 + self._b) - alph1 * k11 - s * alph2 * k12
+            f2 = y2 * (E2 + self._b) - s * alph1 * k12 - alph2 * k22
+            L1 = alph1 + s * (alph2 - L)
+            H1 = alph1 + s * (alph2 - H)
+            Psi_L = L1 * f1 + L * f2 + 0.5 * L1 * L1 * \
+                k11 + 0.5 * L * L * k22 + s * L * L1 * k12
+            Psi_H = H1 * f1 + H * f2 + 0.5 * H1 * H1 * \
+                k11 + 0.5 * H * H * k22 + s * H * H1 * k12
+            if Psi_L < Psi_H:
                 a2 = L
             else:
                 a2 = H
-        if abs(a2-alph2) < self._eps:
+        if abs(a2 - alph2) < self._eps:
             return 0
 
-        a1 = alph1+s*(alph2-a2)
+        a1 = alph1 + s * (alph2 - a2)
 
         # store alpha
         self._alphas[i1] = a1
@@ -84,32 +95,32 @@ class MySVM:
 
         b1 = E1 + y1 * (a1 - alph1) * k11 + y2 * (a2 - alph2) * k12 + self._b
         b2 = E2 + y1 * (a1 - alph1) * k12 + y2 * (a2 - alph2) * k22 + self._b
-        self._b = (b1+b2)/2
+        self._b = (b1 + b2) / 2
         return 1
 
-    def _examineExample(self, i2: int):
+    def _examine_example(self, i2: int):
         X2 = self._features[i2]
         y2 = self._labels[i2]
         alpha2 = self._alphas[i2]
-        E2 = self._calcU(X2) - y2
-        r2 = E2*y2
+        E2 = self._calc_u1(i2) - y2
+        r2 = E2 * y2
         if (r2 < -self._tol and alpha2 < self._C) or (r2 > self._tol and alpha2 > 0.0):
             i1 = -1
-            deltaE = 0.0
+            delta_E = 0.0
             for i in range(self._alphas.size):
-                yTemp = self._alphas[i]
-                alphaTemp = self._alphas[i]
-                if alphaTemp > 0.0 and alphaTemp < self._C:
-                    ETemp = self._calcU(X2) - yTemp
-                    deltaETemp = abs(ETemp - E2)
-                    if deltaETemp > deltaE:
+                y_temp = self._alphas[i]
+                alpha_temp = self._alphas[i]
+                if 0.0 < alpha_temp < self._C:
+                    E_temp = self._calc_u1(i2) - y_temp
+                    delta_E_temp = abs(E_temp - E2)
+                    if delta_E_temp > delta_E:
                         i1 = i
-                        deltaE = deltaETemp
+                        delta_E = delta_E_temp
             if i1 != -1:
-                if self._takeStep(i1, i2) == 1:
+                if self._take_step(i1, i2) == 1:
                     return 1
             for i in range(self._alphas.size):
-                if self._takeStep(i1, i2) == 1:
+                if self._take_step(i1, i2) == 1:
                     return 1
         return 0
 
@@ -117,23 +128,34 @@ class MySVM:
         # initialize
         self._eps = 1e-6
         self._b = 0.0
-        self._alphas = np.zeros(self._labels.size)
+        self._alphas = np.zeros(self._labels.size, dtype=float)
+        self._kxx = np.zeros((self._labels.size, self._labels.size), dtype=float)
+
+        for i in range(self._labels.size):
+            for j in range(i, self._labels.size):
+                self._kxx[i, j] = self._kxx[j, i] = self._kernel_compute(
+                    self._features[i], self._features[j])
 
         # main routine
-        numChanged = 0
-        examineAll = 1
-        while numChanged > 0 or examineAll == 1:
-            numChanged = 0
-            if examineAll == 1:
+        num_changed = 0
+        examine_all = 1
+        iter_time = 0
+        while num_changed > 0 or examine_all == 1:
+            num_changed = 0
+            if examine_all == 1:
                 for i in range(self._alphas.size):
-                    numChanged += self._examineExample(i)
+                    num_changed += self._examine_example(i)
             else:
                 for i in range(self._alphas.size):
-                    alphaI = self._alphas[i]
-                    if alphaI > 0 and alphaI < self._C:
-                        numChanged += self._examineExample(i)
+                    alpha_i = self._alphas[i]
+                    if 0 < alpha_i < self._C:
+                        num_changed += self._examine_example(i)
 
-            if examineAll == 1:
-                examineAll = 0
-            elif numChanged == 0:
-                examineAll = 1
+            if examine_all == 1:
+                examine_all = 0
+            elif num_changed == 0:
+                examine_all = 1
+            iter_time += 1
+            print(iter_time)
+            if iter_time == self._max_iter:
+                break
